8c44280b944474f13cf28a082c92172aeaf44b00
manabe <ayga3355@chiba-u.jp>
2023-05-24 01:54:19 +0900

    feature/add-runtime

---
+++ src/RunTime/var.h
@@ -0,0 +1,24 @@
+#ifndef _VAR_
+#define _VAR_
+
+#include <string>
+#include <iostream>
+
+class Var {
+    private:
+        int intValue;
+        std::string stringValue;
+        int type;
+    public:
+        Var(int intValue);
+        Var(std::string stringValue);
+        ~Var();
+        int getIntValue(void);
+        std::string getStringValue(void);
+        void setIntValue(int intValue);
+        void setStringValue(std::string stringValue);
+        int getType(void);
+        std::string __str(void);
+};
+
+#endif
